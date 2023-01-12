36915045d9586935443e5f2160eee1703ef9dd16
dreywesson <dreywesson@gmail.com>
2023-01-12 11:14:14 +0100

    ft_split: partially working copy

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 11:27:22 by doduwole          #+#    #+#             */
-/*   Updated: 2023/01/12 08:54:25 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/01/12 11:13:41 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 #include "libft.h"
 
 char	**ft_split(char const *s, char c);
-int		ft_delimiter_counter(const char *s, char c);
-void	ft_substr_allocator(char **ptr, int *delimiter_counter, char *buffer);
-void	ft_reset(int *delimiter_counter, int *substr_idx);
 
-int	ft_delimiter_counter(const char *s, char c)
+char	**ft_mem_allocator(const char *s, char c)
 {
 	int		itr;
 	int		delimiter_counter;
+	char	**ptr;
 
 	itr = 0;
 	delimiter_counter = 0;
@@ -31,7 +29,10 @@ int	ft_delimiter_counter(const char *s, char c)
 			delimiter_counter++;
 		itr++;
 	}
-	return (delimiter_counter);
+	ptr = malloc(delimiter_counter + 2);
+	if (!ptr)
+		return (NULL);
+	return (ptr);
 }
 
 void	ft_substr_allocator(char **ptr, int *delimiter_counter, char *buffer)
@@ -43,12 +44,12 @@ void	ft_substr_allocator(char **ptr, int *delimiter_counter, char *buffer)
 void	ft_copy_substr(char *buffer, const char *s, int *substr_idx, int *itr)
 {
 		buffer[*substr_idx] = s[*itr];
-		*substr_idx = *substr_idx + 1;
+		*substr_idx += 1;
 }
 
 void	ft_reset(int *delimiter_counter, int *substr_idx)
 {
-	*delimiter_counter = *delimiter_counter + 1;
+	*delimiter_counter += 1;
 	*substr_idx = 0;
 }
 
@@ -60,22 +61,24 @@ char	**ft_split(char const *s, char c)
 	char	**ptr;
 	char	buffer[16384];
 
-	itr = -1;
+	itr = 0;
 	substr_idx = 0;
-	ptr = malloc(ft_delimiter_counter(s, c));
-	while (s[itr++] != '\0')
+	delimiter_counter = 0;
+	ptr = ft_mem_allocator(s, c);
+	if (!ptr)
+		return (NULL);
+	while (s[itr] != '\0')
 	{
+		buffer[substr_idx] = '\0';
+		ft_substr_allocator(ptr, &delimiter_counter, buffer);
 		if (s[itr] == c)
 		{
-			if (itr == 0)
-				continue ;
-			buffer[substr_idx] = '\0';
-			ft_substr_allocator(ptr, &delimiter_counter, buffer);
-			ft_reset(&delimiter_counter, &substr_idx);
-			continue ;
+			if (itr != 0)
+				ft_reset(&delimiter_counter, &substr_idx);
 		}
-		ft_substr_allocator(ptr, &delimiter_counter, buffer);
-		ft_copy_substr(buffer, s, &substr_idx, &itr);
+		else
+			ft_copy_substr(buffer, s, &substr_idx, &itr);
+		itr++;
 	}
 	ptr[delimiter_counter] = NULL;
 	return (ptr);
@@ -87,7 +90,7 @@ int	main(void)
 	int		i;
 
 	i = 0;
-	ptr = ft_split("mississippi", 'i');
+	ptr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ');
 	while (ptr[i] != NULL)
 	{
 		printf("%s\n", *(ptr + i));
